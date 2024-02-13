e82dcc29095da128a8faa70b5a3c7517d69852e9
mamalhb7 <159778515+mamalhb7@users.noreply.github.com>
2024-02-13 10:09:56 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+import os
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import Client
+
+account_sid = os.environ['AC2e35e932c8d2cce0674ff1ff75eb79ce']
+auth_token = os.environ['9e9cf21f77653bb40fde54a1a61f4b18']
+client = Client(account_sid, auth_token)
 
 app = Flask(__name__)
 
